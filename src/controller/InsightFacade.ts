3bb9a78a61086333a516d4db38db7fa8d6243329
Sei-Young Lee <ubcmush99@gmail.com>
2021-02-10 10:43:42 -0300

    feat(reverted insightfacade changes)

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -37,8 +37,9 @@ export default class InsightFacade implements IInsightFacade {
         }
         Log.trace("InsightFacadeImpl::init()");
     }
+
     private notValidIDRemove = (id: string) => {
-        if (id.includes("_") || id.includes("*")) {
+        if (id.includes("_")) {
             return true;
         } else {
             if (id.trim().length === 0) {
@@ -48,8 +49,12 @@ export default class InsightFacade implements IInsightFacade {
             }
         }
     }
+
     private notValidID = (id: string, kind: InsightDatasetKind) => {
         if (kind === InsightDatasetKind.Rooms) {
+            return true;
+        }
+        if (id.includes("_")) {
             return true;
         } else {
             if (id in this.dict) {
@@ -59,135 +64,91 @@ export default class InsightFacade implements IInsightFacade {
             }
         }
     }
+
     private writeIntoDisc = () => {
         const stringData = JSON.stringify(this.dict);
         const writeDir = path.join(this.cacheDir, "./disc.json");
         fs.writeFileSync( writeDir, stringData);
     }
-    private isNumber = (val: any) => {
-        return(typeof (val) === "number");
-    }
-    private isString = (val: any) => {
-        return(typeof (val) === "string");
-    }
-    private stringNumeric = (val: string) => {
-        return /^\d+$/.test(val);
-    }
-    private datasetOrganizer = (key: string, translatedValues: any, value: number | string) => {
-        switch (key) {
-            case "Course":
-                translatedValues["id"] = value;
-                return this.isString(value) ? translatedValues : false;
-            case "id":
-                translatedValues["uuid"] = `${value}`;
-                return this.isNumber(value) ? translatedValues : false;
-            case "Subject":
-                translatedValues["dept"] = value;
-                return this.isString(value) ? translatedValues : false;
-            case "Professor":
-                translatedValues["instructor"] = value;
-                return this.isString(value) ? translatedValues : false;
-            case "Avg":
-                translatedValues["avg"] = value;
-                return this.isNumber(value) ? translatedValues : false;
-            case "Title":
-                translatedValues["title"] = value;
-                return this.isString(value) ? translatedValues : false;
-            case "Fail":
-                translatedValues["fail"] = value;
-                return this.isNumber(value) ? translatedValues : false;
-            case "Pass":
-                translatedValues["pass"] = value;
-                return this.isNumber(value) ? translatedValues : false;
-            case "Audit":
-                translatedValues["audit"] = value;
-                return this.isNumber(value) ? translatedValues : false;
-            case "Year":
-                translatedValues["year"] = value;
-                return this.isString(value) && this.stringNumeric(typeof value === "string" ? value : "not") ?
-                    translatedValues :
-                    false;
-            default:
-                throw new Error();
-        }
-    }
-    private jsonContentParser = (jsonContent: any, allData: {changed: boolean; values: ObjectValues[]}) => {
-        const keys = ["Course", "Avg", "Professor", "Title", "Pass", "Fail", "Audit", "id", "Year", "Subject"];
-        if ("result" in jsonContent) {
-            if (!(jsonContent.result.length === 0)) {
-                jsonContent.result.forEach((values: any) => {
-                    let translatedValues: any = {};
-                    for (const key of keys) {
-                        if (key in values) {
-                            if (key === "Year") {
-                                if (values["Section"] === "overall") {
-                                    translatedValues = this.datasetOrganizer(key, translatedValues, "1900");
-                                } else {
-                                    translatedValues = this.datasetOrganizer(key, translatedValues, values[key]);
-                                }
-                            } else {
-                                translatedValues = this.datasetOrganizer(key, translatedValues, values[key]);
-                            }
-                            if (!translatedValues) {
-                                translatedValues = {};
-                                break;
-                            }
-                        } else {
-                            translatedValues = {};
-                            break;
-                        }
-                    }
-                    if (Object.keys(translatedValues).length !== 0) {
-                        allData.changed = true;
-                        allData["values"].push(translatedValues);
-                    }
-                });
-            }
-        }
-        return allData;
-    }
+
     private checkAllKeysCourses = (results: any[]) => {
         let allData: {changed: boolean; values: ObjectValues[]} = {
             changed: false, values: []
         };
+        const keys = ["Course", "Avg", "Professor", "Title", "Pass", "Fail", "Audit", "id", "Year", "Subject"];
         results.forEach((r) => {
             if (r.includes("result")) {
                 const jsonContent = JSON.parse(r);
-                allData = this.jsonContentParser(jsonContent, allData);
+                if ("result" in jsonContent) {
+                    if (!(jsonContent.result.length === 0)) {
+                        jsonContent.result.forEach((values: any) => {
+                            let translatedValues: any = {};
+                            for (const key of keys) {
+                                if (key in values) {
+                                    switch (key) {
+                                        case "Course":
+                                            translatedValues["id"] = values[key];
+                                            break;
+                                        case "id":
+                                            translatedValues["uuid"] = values[key];
+                                            break;
+                                        case "Subject":
+                                            translatedValues["dept"] = values[key];
+                                            break;
+                                        case "Professor":
+                                            translatedValues["instructor"] = values[key];
+                                            break;
+                                        default:
+                                            translatedValues[key.toLowerCase()] = values[key];
+                                            break;
+                                    }
+                                } else {
+                                    translatedValues = {};
+                                    break;
+                                }
+                            }
+                            if (Object.keys(translatedValues).length !== 0) {
+                                allData.changed = true;
+                                allData["values"].push(translatedValues);
+                            }
+                        });
+                    }
+                }
             }
         });
         return allData;
     }
+
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         return new Promise<string[]>((resolve, reject) => {
             if (this.notValidID(id, kind)) {
-                return reject(new InsightError());
+                reject(new InsightError());
             } else {
                 let Zip = new JSZip();
                 return Zip.loadAsync(content, {base64: true})
-                .then((result) => {
-                    let promises: Array<Promise<any>> = [];
-                    result.folder("courses").forEach((relativePath, file) => {
-                        promises.push(file.async("string"));
+                    .then((result) => {
+                        let promises: Array<Promise<any>> = [];
+                        result.folder("courses").forEach((relativePath, file) => {
+                            promises.push(file.async("string"));
+                        });
+                        return Promise.all(promises).then((results) =>  {
+                            return this.checkAllKeysCourses(results);
+                        });
+                    }).then((data) => {
+                        if (data.changed) {
+                            this.dict[id] = {
+                                sections: data.values,
+                                type: kind
+                            };
+                            this.writeIntoDisc();
+                            return resolve(Object.keys(this.dict));
+                        } else {
+                            reject(new InsightError());
+                        }
+                    }).catch((e) => {
+                        Log.trace(e);
+                        reject(new InsightError());
                     });
-                    return Promise.all(promises).then((results) =>  {
-                        return this.checkAllKeysCourses(results);
-                    });
-                }).then((data) => {
-                    if (data.changed) {
-                        this.dict[id] = {
-                            sections: data.values,
-                            type: kind
-                        };
-                        this.writeIntoDisc();
-                        return resolve(Object.keys(this.dict));
-                    } else {
-                       return reject(new InsightError());
-                    }
-                }).catch((e) => {
-                    Log.trace(e);
-                    reject(new InsightError());
-                });
             }
         });
     }
--- src/controller/performQueryClass.ts
+++ src/controller/performQueryClass.ts
@@ -1,5 +1,6 @@
 import Log from "../Util";
 import { InsightError, QueryValues, ResultTooLargeError } from "./IInsightFacade";
+import ValidateDataset from "./ValidateDataset";
 
 export default class PerformQueryClass {
     private readonly dict: any;
@@ -7,35 +8,12 @@ export default class PerformQueryClass {
         this.dict = dict;
     }
     private checkOuterQuery = (query: any) => {
-        if (this.isObject(query) && query !== null) {
-            const queryKeys = Object.keys(query);
-            if (queryKeys.includes("WHERE") && queryKeys.includes("OPTIONS") && queryKeys.length === 2) {
-                const options: any = query["OPTIONS"];
-                if (!this.isObject(options)) {
-                    throw new InsightError("outer");
-                }
-                const optionsKeys = Object.keys(options);
-                let length;
-                if ("ORDER" in options) {
-                    length = 2;
-                } else {
-                    length = 1;
-                }
-                if (!(optionsKeys.length === length && "COLUMNS" in options)) {
-                    throw new InsightError("outer 2");
-                } else {
-                    if (!Array.isArray(options["COLUMNS"])) {
-                        throw new InsightError("outer 3");
-                    } else if (length === 2 && !options["COLUMNS"].includes(options["ORDER"])) {
-                        throw new InsightError("outer 4");
-                    }
-                    return length === 2;
-                }
-            } else {
-                throw new InsightError("outer 5");
-            }
+        const validate = new ValidateDataset();
+        const valid: boolean = validate.checkQuery(query, this.dict);
+        if (!valid) {
+            throw new InsightError("not valid");
         } else {
-            throw new InsightError("outer 6");
+            return query["OPTIONS"].hasOwnProperty("ORDER");
         }
     }
     private getColumnsAndDataSet = (columns: string[]) => {
@@ -163,57 +141,39 @@ export default class PerformQueryClass {
         const greaterObject = query.query["GT"];
         const val = this.numberQueryValid(greaterObject, query.id);
         let ret: any[] = [];
-        if (not) {
-            sections.forEach((section: any) => {
-                if (section[val[0]] <= greaterObject[val[1]]) {
-                    ret.push(section);
-                }
-            });
-        } else {
-            sections.forEach((section: any) => {
-                if (section[val[0]] > greaterObject[val[1]]) {
-                    ret.push(section);
-                }
-            });
-        }
+        sections.forEach((section: any) => {
+            if (not && section[val[0]] <= greaterObject[val[1]]) {
+                ret.push(section);
+            } else if (!not && section[val[0]] > greaterObject[val[1]]) {
+                ret.push(section);
+            }
+        });
         return ret;
     }
     private lessQuery(query: QueryValues, not: boolean, sections: any[]) {
         const lessObject = query.query["LT"];
         const val = this.numberQueryValid(lessObject, query.id);
         let ret: any[] = [];
-        if (not) {
-            sections.forEach((section: any) => {
-                if (section[val[0]] >= lessObject[val[1]]) {
-                    ret.push(section);
-                }
-            });
-        } else {
-            sections.forEach((section: any) => {
-                if (section[val[0]] < lessObject[val[1]]) {
-                    ret.push(section);
-                }
-            });
-        }
+        sections.forEach((section: any) => {
+            if (not && section[val[0]] >= lessObject[val[1]]) {
+                ret.push(section);
+            } else if (!not && section[val[0]] < lessObject[val[1]]) {
+                ret.push(section);
+            }
+        });
         return ret;
     }
     private equalQuery(query: QueryValues, not: boolean, sections: any[]) {
         const eqObject = query.query["EQ"];
         const val = this.numberQueryValid(eqObject, query.id);
         let ret: any[] = [];
-        if (not) {
-            sections.forEach((section: any) => {
-                if (section[val[0]] !== eqObject[val[1]]) {
-                    ret.push(section);
-                }
-            });
-        } else {
-            sections.forEach((section: any) => {
-                if (section[val[0]] === eqObject[val[1]]) {
-                    ret.push(section);
-                }
-            });
-        }
+        sections.forEach((section: any) => {
+            if (not && section[val[0]] !== eqObject[val[1]]) {
+                ret.push(section);
+            } else if (!not && section[val[0]] === eqObject[val[1]]) {
+                ret.push(section);
+            }
+        });
         return ret;
     }
     private isObject = (obj: any) => {
@@ -233,29 +193,19 @@ export default class PerformQueryClass {
     private getQuery(query: QueryValues, not: boolean, start: boolean, sections: any[]): any[] {
         if (this.isObject(query.query)) {
             const keys = Object.keys(query.query);
-            if (start && keys.length === 0) {
-                return sections;
-            } else if (keys.length === 0) {
-                throw new InsightError("get query");
+            if (start && keys.length === 0) { return sections;
+            } else if (keys.length === 0) { throw new InsightError("get query");
             } else {
                 if ("NOT" in query.query) {
                     const newQuery: QueryValues = { query: query.query["NOT"], columns: query.columns, id: query.id };
                     return this.getQuery(newQuery, !not, false, sections);
-                } else if ("AND" in query.query) {
-                    return this.OrAndQuery(query, not, "AND", sections);
-                } else if ("OR" in query.query) {
-                    return this.OrAndQuery(query, not, "OR", sections);
-                } else if ("IS" in query.query) {
-                    return this.isQuery(query, not, sections);
-                } else if ("LT" in query.query) {
-                    return this.lessQuery(query, not, sections);
-                } else if ("EQ" in query.query) {
-                    return this.equalQuery(query, not, sections);
-                } else if ("GT" in query.query) {
-                    return this.greaterQuery(query, not, sections);
-                } else {
-                    throw new InsightError("get query 3");
-                }
+                } else if ("AND" in query.query) { return this.OrAndQuery(query, not, "AND", sections);
+                } else if ("OR" in query.query) { return this.OrAndQuery(query, not, "OR", sections);
+                } else if ("IS" in query.query) { return this.isQuery(query, not, sections);
+                } else if ("LT" in query.query) { return this.lessQuery(query, not, sections);
+                } else if ("EQ" in query.query) { return this.equalQuery(query, not, sections);
+                } else if ("GT" in query.query) { return this.greaterQuery(query, not, sections);
+                } else { throw new InsightError("get query 3"); }
             }
         } else {
             throw new InsightError("get query 2");
@@ -266,12 +216,16 @@ export default class PerformQueryClass {
             try {
                 const hasOrder = this.checkOuterQuery(query);
                 const columnsForSections = this.getColumnsAndDataSet(query["OPTIONS"]["COLUMNS"]);
-                const myQuery: QueryValues = { query: query["WHERE"],  columns: columnsForSections["columns"],
+                const myQuery: QueryValues = {
+                    query: query["WHERE"],
+                    columns: columnsForSections["columns"],
                     id: columnsForSections["datasetID"]
                 };
                 const sections = this.dict[columnsForSections["datasetID"]].sections;
                 const theQuery = this.getQuery(myQuery, false, true, sections);
-                if (theQuery.length >= 5000) { return reject(new ResultTooLargeError()); }
+                if (theQuery.length >= 5000) {
+                    return reject(new ResultTooLargeError());
+                }
                 const sectionsRightKeys = this.getRightKeys(myQuery, theQuery);
                 if (hasOrder) {
                     const orderKey: string = query["OPTIONS"]["ORDER"];
@@ -279,10 +233,14 @@ export default class PerformQueryClass {
                         if (orderKey.includes("dept") || orderKey.includes("id") || orderKey.includes("instructor")
                             || orderKey.includes("uuid") || orderKey.includes("title")) {
                             return a[orderKey] > b[orderKey] ? 1 : a[orderKey] < b[orderKey] ? -1 : 0;
-                        } else { return a[orderKey] - b[orderKey]; }
+                        } else {
+                            return a[orderKey] - b[orderKey];
+                        }
                     }));
-                    resolve(orderSectionRight);
-                } else { return resolve(sectionsRightKeys); }
+                    return resolve(orderSectionRight);
+                } else {
+                    return resolve(sectionsRightKeys);
+                }
             } catch (e) {
                 reject(e);
             }
